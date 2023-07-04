e0280db1488005cf6ad4bbd97e5b6ca458b01af6
Jose Jesus Ochoa Torres <jose.jesus.ochoa@wizeline.com>
2023-07-04 00:59:14 -0600

    fix(prettier): updating tabulation width

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -40,9 +40,7 @@ export const Button = ({
 	return (
 		<button
 			type="button"
-			className={["storybook-button", `storybook-button--${size}`, mode].join(
-				" "
-			)}
+			className={["storybook-button", `storybook-button--${size}`, mode].join(" ")}
 			style={{ backgroundColor }}
 			{...props}
 		>
--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -57,12 +57,7 @@ export const Header = ({
 				) : (
 					<>
 						<Button size="small" onClick={onLogin} label="Log in" />
-						<Button
-							primary
-							size="small"
-							onClick={onCreateAccount}
-							label="Sign up"
-						/>
+						<Button primary size="small" onClick={onCreateAccount} label="Sign up" />
 					</>
 				)}
 			</div>
